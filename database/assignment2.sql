f4d0ae46f887632a018c89fff39101c6d12d809c
George Olwal <olwalgeorge@live.com>
2025-05-24 00:40:33 +0300

    Task 5: Use INNER JOIN

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -39,3 +39,10 @@ REPLACE (
 WHERE
     inv_make = 'GM'
     AND inv_model = 'Hummer';
+
+-- Task 5: Use INNER JOIN to select make, model, and classification name for "Sport" category
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM public.inventory i
+    INNER JOIN public.classification c ON i.classification_id = c.classification_id
+WHERE
+    c.classification_name = 'Sport';
