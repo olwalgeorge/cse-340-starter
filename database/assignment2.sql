2e5aa29c766776c03aef15b2beea5e53a44c07d8
George Olwal <olwalgeorge@live.com>
2025-05-24 00:26:25 +0300

    task 2: updated account type of Tony Stark

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,4 +1,4 @@
--- Task: Insert new account record for Tony Stark
+-- Task 1: Insert new account record for Tony Stark
 -- Note: account_id and account_type will handle their own values (auto-generated and default)
 
 INSERT INTO
@@ -14,3 +14,11 @@ VALUES (
         'tony@starkent.com',
         'Iam1ronM@n'
     );
+
+-- Task 2 : Update Tony Stark's account to change account_type to "Admin", use email as unique identiier
+UPDATE public.account
+SET
+    account_type = 'Admin'
+WHERE
+    account_email = 'tony@starkent.com';
+
